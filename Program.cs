db45b5aaed0461787ea29dd9d9a7386b73bf4586
Will Schick <will.schick@marketnews.com>
2021-02-27 16:41:20 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AlphaFlashSelectClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HttpClient httpClient = new HttpClient();
+
+           
+
+
+        }
+    }
+}
